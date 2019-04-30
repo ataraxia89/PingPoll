df4dcd7e84a351449af510455bd3e98fdb593333
John Foster <johnfoster1989+github@gmail.com>
2019-04-30 10:11:57 +0100

    Code tidy up

--- PingPoll/Form1.cs
+++ PingPoll/Form1.cs
@@ -9,6 +9,8 @@ namespace PingPoll
 {
     public partial class Form1 : Form
     {
+        #region Private Members
+
         private System.Timers.Timer pingTimer;
         private System.Timers.Timer ipTimer;
         private System.Net.NetworkInformation.Ping ping;
@@ -22,6 +24,10 @@ namespace PingPoll
         private DateTime? offlineTimestamp;
         private DateTime onlineTimestamp;
         private bool codeRunning;
+
+        #endregion
+
+        #region Constructor
 
         public Form1()
         {
@@ -39,6 +45,10 @@ namespace PingPoll
 
             lblCurrentIP.Text = "";
         }
+
+        #endregion
+
+        #region Timers
 
         private void pingTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
@@ -124,6 +134,16 @@ namespace PingPoll
             ipTimer.Enabled = true;
         }
 
+        #endregion
+
+        #region Form Events/Functions
+
+        private void dgv_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (!codeRunning)
+                chkLiveScrolling.Checked = false;
+        }
+
         private void btnStartStop_Click(object sender, EventArgs e)
         {
             if (int.TryParse(txtPingInterval.Text, out interval))
@@ -152,6 +172,80 @@ namespace PingPoll
             }
         }
 
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgvEvents.Rows.Clear();
+            dgvOffline.Rows.Clear();
+        }
+
+        private void btnExportEvents_Click(object sender, EventArgs e)
+        {
+            if (dgvEvents.Rows.Count > 0)
+                ExportDataGridViewToCSV(dgvEvents, "EventLog.csv");
+            else
+                MessageBox.Show("No event data to export!");
+        }
+
+        private void btnExportDowntime_Click(object sender, EventArgs e)
+        {
+            if (dgvOffline.Rows.Count > 0)
+                ExportDataGridViewToCSV(dgvOffline, "DowntimeLog.csv");
+            else
+                MessageBox.Show("No downtime data to export!");
+        }
+
+        private void ExportDataGridViewToCSV(DataGridView dgv, string defaultFileName = "")
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = defaultFileName;
+            saveFile.Filter = "CSV file (*.csv)|*.csv";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string delimiter = ",";
+
+                int columnCount = dgv.Columns.Count;
+
+                StringBuilder sb = new StringBuilder();
+
+                string[] columnHeaders = new string[columnCount];
+                for (int i = 0; i < columnCount; i++)
+                    columnHeaders[i] = dgv.Columns[i].HeaderText;
+
+                sb.AppendLine(string.Join(delimiter, columnHeaders));
+
+                string[] rowData = new string[columnCount];
+                foreach (DataGridViewRow row in dgv.Rows)
+                {
+                    for (int i = 0; i < columnCount; i++)
+                        rowData[i] = row.Cells[i].Value.ToString();
+
+                    sb.AppendLine(string.Join(delimiter, rowData));
+                }
+
+                File.WriteAllText(saveFile.FileName, sb.ToString());
+
+                MessageBox.Show($"'{saveFile.FileName}' saved successfully");
+            }
+        }
+
+        private delegate void UpdateCurrentIPDelegate(Label lbl, string newIP);
+
+        private void UpdateCurrentIP(Label lbl, string newIP)
+        {
+            if (lbl.InvokeRequired)
+                lbl.Invoke(new UpdateCurrentIPDelegate(UpdateCurrentIP), lbl, newIP);
+            else
+            {
+                lbl.Text = newIP;
+                Update();
+            }
+        }
+
+        #endregion
+
+        #region DataGridView Logging
+
         private delegate void AddEventLogDelegate(DataGridView dgv, string eventType, string eventDetails);
 
         private void AddEventLog(DataGridView dgv, string eventType, string eventDetails)
@@ -170,7 +264,7 @@ namespace PingPoll
                     dgv.FirstDisplayedScrollingRowIndex = dgv.Rows.Count - 1;
                     codeRunning = false;
                 }
-                
+
                 Update();
             }
         }
@@ -199,80 +293,6 @@ namespace PingPoll
             }
         }
 
-        private delegate void UpdateCurrentIPDelegate(Label lbl, string newIP);
-
-        private void UpdateCurrentIP(Label lbl, string newIP)
-        {
-            if (lbl.InvokeRequired)
-                lbl.Invoke(new UpdateCurrentIPDelegate(UpdateCurrentIP), lbl, newIP);
-            else
-            {
-                lbl.Text = newIP;
-                Update();
-            }
-        }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            dgvEvents.Rows.Clear();
-            dgvOffline.Rows.Clear();
-        }
-
-        private void btnExportEvents_Click(object sender, EventArgs e)
-        {
-            if (dgvEvents.Rows.Count > 0)
-                ExportDataGridViewToCSV(dgvEvents, "EventLog.csv");
-            else
-                MessageBox.Show("No event data to export!");
-        }
-
-        private void btnExportDowntime_Click(object sender, EventArgs e)
-        {
-            if (dgvOffline.Rows.Count > 0)
-                ExportDataGridViewToCSV(dgvOffline, "DowntimeLog.csv");
-            else
-                MessageBox.Show("No downtime data to export!");
-        }
-
-        private void dgv_Scroll(object sender, ScrollEventArgs e)
-        {
-            if (!codeRunning)
-                chkLiveScrolling.Checked = false;
-        }
-
-        private void ExportDataGridViewToCSV(DataGridView dgv, string defaultFileName = "")
-        {
-            SaveFileDialog saveFile = new SaveFileDialog();
-            saveFile.FileName = defaultFileName;
-            saveFile.Filter = "CSV file (*.csv)|*.csv";
-
-            if (saveFile.ShowDialog() == DialogResult.OK)
-            {
-                string delimiter = ",";
-
-                int columnCount = dgv.Columns.Count;
-
-                StringBuilder sb = new StringBuilder();
-
-                string[] columnHeaders = new string[columnCount];
-                for (int i = 0; i < columnCount; i++)
-                    columnHeaders[i] = dgv.Columns[i].HeaderText;
-
-                sb.AppendLine(string.Join(delimiter, columnHeaders));
-                
-                string[] rowData = new string[columnCount];
-                foreach (DataGridViewRow row in dgv.Rows)
-                {
-                    for (int i = 0; i < columnCount; i++)
-                        rowData[i] = row.Cells[i].Value.ToString();
-
-                    sb.AppendLine(string.Join(delimiter, rowData));
-                }
-
-                File.WriteAllText(saveFile.FileName, sb.ToString());
-
-                MessageBox.Show($"'{saveFile.FileName}' saved successfully");
-            }
-        }
+        #endregion        
     }
 }
